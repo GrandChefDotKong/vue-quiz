6f04d79bc98f0fef6a94ef607340faca0f5d1bf0
Adrien Pauchet <grandchefdotkong@gmail.com>
2022-11-11 19:20:05 +0900

    result components added

---
+++ src/components/Result.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="result">
+    <h1>You results</h1>
+    <p>
+      {{ numberOfCorrectAnswers }} correct answers
+      on {{ numberOfQuestions }} questions.
+    </p>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+  const { numberOfCorrectAnswers, numberOfQuestions } = 
+  defineProps(['numberOfCorrectAnswers', 'numberOfQuestions']);
+
+</script>
+
+<style scoped>
+ .result {
+  text-align: center;
+  padding: 100px 0;
+ }
+ p {
+  font-size: 25px;
+ }
+ h1 {
+  font-size: 60px;
+ }
+</style>
--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,13 +1,64 @@
 <template>
   <div>
-    <h2>Hello</h2>
+    <QuizHeader 
+      :berPercentage="berPercentage" 
+      :questionStatus="questionStatus"
+    />
+    <div v-if="!showResult">
+      <Question 
+        :question="quiz.questions[currentQuestionIndex]" 
+        @selectOption="onOptionSelected"
+      />
+    </div>
+    <div v-else>
+      <Result 
+        :numberOfCorrectAnswers="numberOfCorrectAnswers" 
+        :numberOfQuestions="quiz.questions.length"
+      />
+    </div>
   </div>
 </template>
 
 <script setup>
+import Question from '../components/Question.vue';
+import QuizHeader from '../components/QuizHeader.vue';
+import Result from '../components/Result.vue';
+import quizes from '../data/quizes.json';
+import { useRoute } from 'vue-router';
+import { computed, ref } from 'vue';
+
+  const route = useRoute();
+
+  const quizId = parseInt(route.params.id);
+
+  const quiz = quizes.find(quiz => quiz.id === quizId);
+  const currentQuestionIndex = ref(0);
+  const numberOfCorrectAnswers = ref(0);
+  const showResult = ref(false);
+
+  const onOptionSelected = (isCorrect) => {
+    if(isCorrect) {
+      numberOfCorrectAnswers.value++;
+    }
+
+    currentQuestionIndex.value++;
+
+    if(currentQuestionIndex.value >= quiz.questions.length) {
+      showResult.value = true;
+    }
+  }
+
+  const questionStatus = 
+    computed(() => `${ currentQuestionIndex.value }/${ quiz.questions.length }`);
+  const berPercentage = 
+    computed(() => `${ currentQuestionIndex.value / quiz.questions.length * 100 }%`);
 
 </script>
 
-<style scoped>
 
-</style>
+
+
+
+
+
+
